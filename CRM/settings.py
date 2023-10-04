31deb2309eb2712ca55e433258ccca1437e79bb2
Jatin Kant <59907081+Jatin666@users.noreply.github.com>
2023-10-04 13:00:07 +0200

    Update settings.py

--- CRM/settings.py
+++ CRM/settings.py
@@ -28,7 +28,6 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -135,16 +134,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# Define the directory where your static files are located
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATIC_URL = 'static/'
 
-# In production, you don't need this block since it's meant for development only
-if DEBUG:
-    STATIC_URL = '/static/'
+if DEBUG :
+    STATICFILES_DIRS = [ 'static/' ]
 else:
-    STATIC_URL = '/static/'  # Adjust as needed for your production server configuration
+    STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -153,7 +149,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'accounts.CustomUserTypes'
-
 
 
 MAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
